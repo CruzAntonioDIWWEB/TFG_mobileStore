14981aa47b5a51f9bb4b5d6d3548b88321742e66
Antonio Cruz García <acg.purullena@gmail.com>
2025-06-15 02:35:11 +0200

    formatting sass

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -5,34 +5,37 @@ namespace config;
 use PDO;
 use PDOException;
 
-class DatabaseConfig{
+class DatabaseConfig
+{
     private $connection;
     private $host;
     private $dbname;
     private $user;
     private $password;
-    
-    public function __construct(){
-        //Cargo las variables de entorno desde el fichero .env
+
+    public function __construct()
+    {
+        // Load the environment variables from .env file
         require_once __DIR__ . '/../vendor/autoload.php';
-        
+
         $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
         $dotenv->load();
-        
-        //Asigno los valores de configuración
+
+        // Set database connection parameters from environment variables
         $this->host = $_ENV['DBHOST'];
         $this->dbname = $_ENV['DBNAME'];
         $this->user = $_ENV['DBUSER'];
         $this->password = $_ENV['DBPASSWORD'];
-        
-        //Se crea la conexión
+
+        // Establish the database connection
         $this->connect();
     }
-    
+
     /**
-     * Se establece la conexión a la base de datos
+     * Connect to the database using PDO
      */
-    private function connect(){
+    private function connect()
+    {
         try {
             $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8";
             $this->connection = new PDO($dsn, $this->user, $this->password);
@@ -42,9 +45,9 @@ class DatabaseConfig{
             die("Error de conexión: " . $e->getMessage());
         }
     }
-    
+
     /**
-     * Se devuelve la conexión a la base de datos
+     * Get the PDO connection instance
      */
     public function getConnection()
     {
